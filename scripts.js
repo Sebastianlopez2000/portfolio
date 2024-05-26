775b3a296be69b6374c91da8042284ff303abd53
Ben S Lopez <144920184+Sebastianlopez2000@users.noreply.github.com>
2024-05-26 01:03:57 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('contactForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    document.getElementById('formResponse').classList.remove('hidden');
-    document.getElementById('formResponse').classList.add('visible');
-});
